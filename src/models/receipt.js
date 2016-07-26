52a68e5c0e5ab516d7c5e87ff1f2e55c80900adc
egrils <egrils@users.noreply.github.com>
2016-07-26 09:00:39 +0800

    move buildReceiptText to receipt.buildText method

--- src/models/receipt.js
+++ src/models/receipt.js
@@ -17,6 +17,31 @@ class Receipt {
 
     return new Receipt(receiptItems, savedTotal, total);
   }
+
+  buildText() {
+
+    const formatMoney = (money) => {
+      return money.toFixed(2);
+    };
+
+    let receiptItemsText = this.receiptItems
+      .map(receiptItem => {
+        const cartItem = receiptItem.cartItem;
+        return `名称：${cartItem.item.name}，\
+数量：${cartItem.count}${cartItem.item.unit}，\
+单价：${formatMoney(cartItem.item.price)}(元)，\
+小计：${formatMoney(receiptItem.subtotal)}(元)`;
+      })
+      .join('\n');
+
+    return `***<没钱赚商店>收据***
+${receiptItemsText}
+----------------------
+总计：${formatMoney(this.total)}(元)
+节省：${formatMoney(this.savedTotal)}(元)
+**********************`;
+  }
+
 }
 
 module.exports = Receipt;
